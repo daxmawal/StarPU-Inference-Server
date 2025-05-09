46dd9c8af12efd48a953f3654674b0b9e9fc5e5b
daxmawal <jeanfrancoismanutea@gmail.com>
2025-05-09 23:01:10 +0200

    indentation

--- src/args_parser.hpp
+++ src/args_parser.hpp
@@ -7,63 +7,63 @@
 
 struct ProgramOptions 
 {
-	std::string scheduler = "lws";
-	int iterations = 1;
-	bool show_help = false;
-	bool valid = true;
+  std::string scheduler = "lws";
+  int iterations = 1;
+  bool show_help = false;
+  bool valid = true;
 };
 
 void display_help(const char* prog_name) 
 {
 	std::cout << "Usage: " << prog_name << " [OPTIONS]\n"
-						<< "\nOptions:\n"
-						<< "  --scheduler [name]    Scheduler type (default: lws)\n"
-						<< "  --iterations [num]    Number of iterations (default: 1)\n"
-						<< "  --help                Show this help message\n";
+            << "\nOptions:\n"
+            << "  --scheduler [name]    Scheduler type (default: lws)\n"
+            << "  --iterations [num]    Number of iterations (default: 1)\n"
+            << "  --help                Show this help message\n";
 }
 
 ProgramOptions parse_arguments(int argc, char* argv[]) 
 {
-	ProgramOptions opts;
+  ProgramOptions opts;
 
-	for (int i = 1; i < argc; ++i) 
-	{
-		std::string arg = argv[i];
+  for (int i = 1; i < argc; ++i) 
+  {
+    std::string arg = argv[i];
 
-		if (arg == "--scheduler" && i + 1 < argc) 
-		{
-			opts.scheduler = argv[++i];
-		}
-		else if (arg == "--iterations" && i + 1 < argc) 
-		{
-			try 
-			{
-				opts.iterations = std::stoi(argv[++i]);
-				if (opts.iterations <= 0) 
-				{
-					throw std::invalid_argument("Iterations must be positive.");
-				}
-			}
-			catch (const std::exception& e) 
-			{
-				std::cerr << "Invalid value for iterations: " << e.what() << "\n";
-				opts.valid = false;
-				return opts;
-			}
-		}
-		else if (arg == "--help") 
-		{
-			opts.show_help = true;
-			return opts;
-		}
-		else 
-		{
-			std::cerr << "Unknown argument: " << arg << "\n";
-			opts.valid = false;
-			return opts;
-		}
-	}
-	return opts;
+    if (arg == "--scheduler" && i + 1 < argc) 
+    {
+      opts.scheduler = argv[++i];
+    }
+    else if (arg == "--iterations" && i + 1 < argc) 
+    {
+      try 
+      {
+        opts.iterations = std::stoi(argv[++i]);
+        if (opts.iterations <= 0) 
+        {
+          throw std::invalid_argument("Iterations must be positive.");
+        }
+      }
+      catch (const std::exception& e) 
+      {
+        std::cerr << "Invalid value for iterations: " << e.what() << "\n";
+        opts.valid = false;
+        return opts;
+      }
+    }
+    else if (arg == "--help") 
+    {
+      opts.show_help = true;
+      return opts;
+    }
+    else 
+    {
+      std::cerr << "Unknown argument: " << arg << "\n";
+      opts.valid = false;
+      return opts;
+    }
+  }
+  return opts;
 }
 
 #endif // ARGS_PARSER_HPP
--- src/starpu_setup.hpp
+++ src/starpu_setup.hpp
@@ -6,39 +6,38 @@
 
 class StarPUSetup
 {
-public:
-	StarPUSetup(const char* sched_policy)
-	{
-		starpu_conf_init(&conf_);
-		conf_.sched_policy_name = sched_policy;
-		if (starpu_init(&conf_) != 0)
-		{
-    		
-			throw std::runtime_error("StarPU initialization error");
-		}
+ public:
+  StarPUSetup(const char* sched_policy)
+  {
+    starpu_conf_init(&conf_);
+    conf_.sched_policy_name = sched_policy;
+    if (starpu_init(&conf_) != 0)
+    {		
+      throw std::runtime_error("StarPU initialization error");
+    }
 
-		starpu_codelet_init(&codelet_);
-		codelet_.cpu_funcs[0] = cpu_codelet_func;
-		codelet_.cpu_funcs_name[0] = "cpu_codelet_func";
-		codelet_.modes[0] = STARPU_RW;
-		codelet_.name = "cpu_double";
-	}
+    starpu_codelet_init(&codelet_);
+    codelet_.cpu_funcs[0] = cpu_codelet_func;
+    codelet_.cpu_funcs_name[0] = "cpu_codelet_func";
+    codelet_.modes[0] = STARPU_RW;
+    codelet_.name = "cpu_double";
+  }
 
-	~StarPUSetup()
-	{
-		starpu_shutdown();
-	}
+  ~StarPUSetup()
+  {
+    starpu_shutdown();
+  }
 
-	struct starpu_codelet* codelet() { return &codelet_; }
+  struct starpu_codelet* codelet() { return &codelet_; }
 
-private:
-	static void cpu_codelet_func(void *buffers[], void *cl_arg)
-	{
-		std::cout << "Hello World" << std::endl;
-	}
+ private:
+  static void cpu_codelet_func(void *buffers[], void *cl_arg)
+  {
+    std::cout << "Hello World" << std::endl;
+  }
 
-	struct starpu_conf conf_;
-	struct starpu_codelet codelet_;
+  struct starpu_conf conf_;
+  struct starpu_codelet codelet_;
 };
 
 #endif // STARPU_SETUP_HPP
