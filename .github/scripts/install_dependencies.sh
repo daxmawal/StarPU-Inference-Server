326cb3e20701a0fb933d5e80d8c412893e701331
daxmawal <jeanfrancoismanutea@gmail.com>
2025-07-17 13:21:22 +0200

    ci: Parrallel build of grpc set to 2 cores

--- .github/scripts/install_dependencies.sh
+++ .github/scripts/install_dependencies.sh
@@ -73,7 +73,7 @@ cmake .. \
   -DBUILD_SHARED_LIBS=OFF \
   -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
   -DCMAKE_BUILD_TYPE=Release
-cmake --build . --parallel
+cmake --build . --parallel 2
 sudo cmake --install .
 cd / && sudo rm -rf /tmp/grpc
 
