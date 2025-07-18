97c4d0c6954b8591d37c20e9aadfe1d8dd31d7c2
daxmawal <jeanfrancoismanutea@gmail.com>
2025-07-18 14:04:26 +0200

    ci: add cmake format

--- cmake/FindTorchCustom.cmake
+++ cmake/FindTorchCustom.cmake
@@ -3,12 +3,18 @@ include_guard(GLOBAL)
 find_package(Torch REQUIRED)
 
 if(NOT TARGET Torch::Torch)
-    message(WARNING "Torch::Torch target not defined by libtorch — creating custom Torch::Torch target.")
-    add_library(Torch::Torch INTERFACE IMPORTED)
-    set_property(TARGET Torch::Torch PROPERTY INTERFACE_LINK_LIBRARIES ${TORCH_LIBRARIES})
-    if(Torch_INCLUDE_DIRS)
-        set_property(TARGET Torch::Torch PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${Torch_INCLUDE_DIRS}")
-    elseif(TORCH_INCLUDE_DIRS)
-        set_property(TARGET Torch::Torch PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${TORCH_INCLUDE_DIRS}")
-    endif()
+  message(
+    WARNING
+      "Torch::Torch target not defined by libtorch — creating custom Torch::Torch target."
+  )
+  add_library(Torch::Torch INTERFACE IMPORTED)
+  set_property(TARGET Torch::Torch PROPERTY INTERFACE_LINK_LIBRARIES
+                                            ${TORCH_LIBRARIES})
+  if(Torch_INCLUDE_DIRS)
+    set_property(TARGET Torch::Torch PROPERTY INTERFACE_INCLUDE_DIRECTORIES
+                                              "${Torch_INCLUDE_DIRS}")
+  elseif(TORCH_INCLUDE_DIRS)
+    set_property(TARGET Torch::Torch PROPERTY INTERFACE_INCLUDE_DIRECTORIES
+                                              "${TORCH_INCLUDE_DIRS}")
+  endif()
 endif()
