cba3db751a1bbb36cc777b41f71891e344d2610a
daxmawal <jeanfrancoismanutea@gmail.com>
2025-08-30 00:36:27 +0200

    refactor: tidy integration utils

--- tests/integration/utils/integration_inference_validator.cpp
+++ tests/integration/utils/integration_inference_validator.cpp
@@ -8,10 +8,17 @@ TEST_F(InferenceValidator_Integration, SuccessfulValidationCuda)
   auto model = starpu_server::make_add_one_model();
   model.to(torch::kCUDA);
   auto opts = torch::TensorOptions().dtype(torch::kLong).device(torch::kCUDA);
-  auto in = torch::arange(1, 4, opts);
-  auto out = torch::arange(2, 5, opts);
+  constexpr int kJobId = 100;
+  constexpr int64_t kInputStart = 1;
+  constexpr int64_t kInputEnd = 4;
+  constexpr int64_t kOutputStart = 2;
+  constexpr int64_t kOutputEnd = 5;
+
+  auto input_tensor = torch::arange(kInputStart, kInputEnd, opts);
+  auto expected_output = torch::arange(kOutputStart, kOutputEnd, opts);
   auto result = starpu_server::make_result(
-      {in}, {out}, 100, starpu_server::DeviceType::CUDA);
+      {input_tensor}, {expected_output}, kJobId,
+      starpu_server::DeviceType::CUDA);
   EXPECT_TRUE(validate_inference_result(
       result, model, starpu_server::VerbosityLevel::Silent));
 }
@@ -22,10 +29,14 @@ TEST_F(InferenceValidator_Integration, FailsOnMismatchCuda_ReturnsFalseAndLogs)
   auto model = starpu_server::make_add_one_model();
   model.to(torch::kCUDA);
   auto opts = torch::TensorOptions().dtype(torch::kLong).device(torch::kCUDA);
-  auto in = torch::arange(1, 4, opts);
+  constexpr int kJobId = 101;
+  constexpr int64_t kInputStart = 1;
+  constexpr int64_t kInputEnd = 4;
+
+  auto input_tensor = torch::arange(kInputStart, kInputEnd, opts);
   auto wrong = torch::arange(1, 4, opts);
   auto res = starpu_server::make_result(
-      {in}, {wrong}, 101, starpu_server::DeviceType::CUDA);
+      {input_tensor}, {wrong}, kJobId, starpu_server::DeviceType::CUDA);
 
   testing::internal::CaptureStderr();
   EXPECT_FALSE(validate_inference_result(
@@ -40,12 +51,16 @@ TEST_F(InferenceValidator_Integration, CudaModelOnCpuInputs_OK)
   auto model = starpu_server::make_add_one_model();
   model.to(torch::kCUDA);
 
+  constexpr int kJobId = 102;
+  constexpr int64_t kExpectedStart = 2;
+  constexpr int64_t kExpectedEnd = 5;
+
   auto res = starpu_server::make_result(
       {torch::tensor({1, 2, 3})},
       {torch::arange(
-          2, 5,
+          kExpectedStart, kExpectedEnd,
           torch::TensorOptions().dtype(torch::kLong).device(torch::kCUDA))},
-      102, starpu_server::DeviceType::CUDA);
+      kJobId, starpu_server::DeviceType::CUDA);
 
   EXPECT_TRUE(validate_inference_result(
       res, model, starpu_server::VerbosityLevel::Silent));
