427b13ee1deb2f4683a4f3eae3a699d9a6f4f760
daxmawal <jeanfrancoismanutea@gmail.com>
2025-09-27 23:20:38 +0200

    test: add unit test for releasing slots back to the pool in OutputSlotPool

--- tests/unit/core/unit_starpu_setup.cpp
+++ tests/unit/core/unit_starpu_setup.cpp
@@ -60,3 +60,36 @@ TEST(OutputSlotPool_Unit, AllocateSlotBuffersOverflowThrows)
   EXPECT_THROW(
       { starpu_server::OutputSlotPool pool(opts, 1); }, std::overflow_error);
 }
+
+TEST(OutputSlotPool_Unit, ReleaseReturnsSlotToPool)
+{
+  StarpuRuntimeGuard starpu_guard;
+
+  starpu_server::RuntimeConfig opts;
+  opts.max_batch_size = 1;
+
+  starpu_server::TensorConfig tensor;
+  tensor.name = "simple_output";
+  tensor.dims = {1, 1};
+  tensor.type = at::ScalarType::Float;
+
+  starpu_server::ModelConfig model;
+  model.name = "simple_model";
+  model.outputs.push_back(tensor);
+  opts.models.push_back(model);
+
+  starpu_server::OutputSlotPool pool(opts, 1);
+
+  const int slot_id = pool.acquire();
+  EXPECT_GE(slot_id, 0);
+
+  EXPECT_FALSE(pool.try_acquire().has_value());
+
+  pool.release(slot_id);
+
+  auto maybe_slot = pool.try_acquire();
+  ASSERT_TRUE(maybe_slot.has_value());
+  EXPECT_EQ(*maybe_slot, slot_id);
+
+  pool.release(*maybe_slot);
+}
