ddc6959a638f7123f67d8d33d1df5fb7e5eb707c
daxmawal <jeanfrancoismanutea@gmail.com>
2025-09-28 22:58:48 +0200

    test: add unit tests for NVTX hooks and tracking functionality

--- src/utils/nvtx.hpp
+++ src/utils/nvtx.hpp
@@ -8,28 +8,87 @@
 
 namespace starpu_server {
 
+namespace detail {
+
+using NvtxPushHook = void (*)(const std::string&);
+using NvtxPopHook = void (*)();
+
+inline void
+DefaultPushHook(const std::string& label)
+{
+#ifdef HAVE_NVTX
+  nvtxRangePushA(label.c_str());
+#else
+  (void)label;
+#endif
+}
+
+inline void
+DefaultPopHook()
+{
+#ifdef HAVE_NVTX
+  nvtxRangePop();
+#endif
+}
+
+inline auto&
+PushHookRef()
+{
+  static NvtxPushHook hook = &DefaultPushHook;
+  return hook;
+}
+
+inline auto&
+PopHookRef()
+{
+  static NvtxPopHook hook = &DefaultPopHook;
+  return hook;
+}
+
+inline void
+SetHooks(NvtxPushHook push, NvtxPopHook pop)
+{
+  PushHookRef() = push ? push : &DefaultPushHook;
+  PopHookRef() = pop ? pop : &DefaultPopHook;
+}
+
+inline void
+InvokePush(const std::string& label)
+{
+  PushHookRef()(label);
+}
+
+inline void
+InvokePop()
+{
+  PopHookRef()();
+}
+
+}  // namespace detail
+
 class NvtxRange {
  public:
+  using PushHook = detail::NvtxPushHook;
+  using PopHook = detail::NvtxPopHook;
+
   explicit NvtxRange(const char* name) : label_(name)
   {
-#ifdef HAVE_NVTX
-    nvtxRangePushA(label_.c_str());
-#endif
+    detail::InvokePush(label_);
   }
 
   explicit NvtxRange(std::string name) : label_(std::move(name))
   {
-#ifdef HAVE_NVTX
-    nvtxRangePushA(label_.c_str());
-#endif
+    detail::InvokePush(label_);
   }
 
-  ~NvtxRange()
+  ~NvtxRange() { detail::InvokePop(); }
+
+  static void SetHooks(PushHook push, PopHook pop)
   {
-#ifdef HAVE_NVTX
-    nvtxRangePop();
-#endif
+    detail::SetHooks(push, pop);
   }
+
+  static void ResetHooks() { detail::SetHooks(nullptr, nullptr); }
 
   NvtxRange(const NvtxRange&) = delete;
   auto operator=(const NvtxRange&) -> NvtxRange& = delete;
---
+++ tests/unit/utils/unit_nvtx.cpp
@@ -0,0 +1,60 @@
+#define HAVE_NVTX 1
+
+#include <gtest/gtest.h>
+
+#include <string>
+
+#include "nvtx3/nvToolsExt.h"
+#include "utils/nvtx.hpp"
+
+using namespace starpu_server;
+
+namespace {
+
+void
+TestPushHook(const std::string&)
+{
+  testing_nvtx::TrackPush();
+}
+
+void
+TestPopHook()
+{
+  testing_nvtx::TrackPop();
+}
+
+class NvtxHookGuard {
+ public:
+  NvtxHookGuard() { NvtxRange::SetHooks(&TestPushHook, &TestPopHook); }
+
+  ~NvtxHookGuard() { NvtxRange::ResetHooks(); }
+};
+
+}  // namespace
+
+TEST(NvtxRange, TracksPushPopWithCString)
+{
+  NvtxHookGuard guard{};
+  testing_nvtx::Reset();
+  {
+    NvtxRange range{"c-string label"};
+    EXPECT_EQ(testing_nvtx::PushCount(), 1);
+    EXPECT_EQ(testing_nvtx::PopCount(), 0);
+  }
+  EXPECT_EQ(testing_nvtx::PushCount(), 1);
+  EXPECT_EQ(testing_nvtx::PopCount(), 1);
+}
+
+TEST(NvtxRange, TracksPushPopWithString)
+{
+  NvtxHookGuard guard{};
+  testing_nvtx::Reset();
+  {
+    std::string label{"string label"};
+    NvtxRange range{label};
+    EXPECT_EQ(testing_nvtx::PushCount(), 1);
+    EXPECT_EQ(testing_nvtx::PopCount(), 0);
+  }
+  EXPECT_EQ(testing_nvtx::PushCount(), 1);
+  EXPECT_EQ(testing_nvtx::PopCount(), 1);
+}
