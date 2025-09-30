2db2d6791d5763b06e650cf55cbec5bf984e56b3
daxmawal <jeanfrancoismanutea@gmail.com>
2025-09-30 16:05:35 +0200

    test: add additional checks for metrics initialization and error handling

--- tests/unit/monitoring/unit_metrics.cpp
+++ tests/unit/monitoring/unit_metrics.cpp
@@ -1,4 +1,8 @@
+#include <arpa/inet.h>
 #include <gtest/gtest.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
 #include <algorithm>
 #include <string_view>
@@ -55,6 +59,41 @@ TEST(Metrics, RepeatedInitDoesNotAllocateRegistry)
   EXPECT_FALSE(init_metrics(0));
   auto second = get_metrics();
   EXPECT_EQ(first, second);
+
+  shutdown_metrics();
+  EXPECT_EQ(get_metrics(), nullptr);
+}
+
+TEST(Metrics, InitFailsWhenMetricsRegistryThrows)
+{
+  shutdown_metrics();
+
+  int reserved_socket = ::socket(AF_INET, SOCK_STREAM, 0);
+  ASSERT_GE(reserved_socket, 0);
+
+  sockaddr_in addr{};
+  addr.sin_family = AF_INET;
+  addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+  addr.sin_port = 0;
+
+  ASSERT_EQ(
+      ::bind(reserved_socket, reinterpret_cast<sockaddr*>(&addr), sizeof(addr)),
+      0);
+  ASSERT_EQ(::listen(reserved_socket, 1), 0);
+
+  socklen_t addr_len = sizeof(addr);
+  ASSERT_EQ(
+      ::getsockname(
+          reserved_socket, reinterpret_cast<sockaddr*>(&addr), &addr_len),
+      0);
+  const int reserved_port = ntohs(addr.sin_port);
+  ASSERT_GT(reserved_port, 0);
+
+  EXPECT_FALSE(init_metrics(reserved_port));
+  EXPECT_EQ(get_metrics(), nullptr);
+
+  ::close(reserved_socket);
+
 
   shutdown_metrics();
   EXPECT_EQ(get_metrics(), nullptr);
