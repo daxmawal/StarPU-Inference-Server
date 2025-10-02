6dc22c87efa236d1f38c8ae1713c7bf5a1682e29
daxmawal <jeanfrancoismanutea@gmail.com>
2025-10-02 23:55:09 +0200

    refactor: enhance runtime configuration tests with parameterized exception handling

--- tests/unit/utils/unit_runtime_config.cpp
+++ tests/unit/utils/unit_runtime_config.cpp
@@ -1,94 +1,165 @@
 #include <gtest/gtest.h>
 
+#include <cstdint>
+#include <functional>
 #include <limits>
+#include <vector>
 
 #include "utils/runtime_config.hpp"
 
-TEST(RuntimeConfig, ComputeMaxMessageBytesThrowsOnNumelOverflow)
+namespace {
+
+struct RuntimeErrorCase {
+  std::function<void(
+      starpu_server::ModelConfig& model,
+      std::vector<starpu_server::TensorConfig>& inputs,
+      std::vector<starpu_server::TensorConfig>& outputs, int64_t& batch_size)>
+      prepare_configs;
+  std::function<void(const std::function<void()>&)> expect_exception;
+  std::function<void(
+      int64_t batch_size, const starpu_server::ModelConfig& model,
+      const std::vector<starpu_server::TensorConfig>& inputs,
+      const std::vector<starpu_server::TensorConfig>& outputs)>
+      callable;
+};
+
+template <typename Exception>
+auto
+make_expect_throw() -> std::function<void(const std::function<void()>&)>
 {
-  starpu_server::TensorConfig tensor_conf;
-  tensor_conf.dims = {std::numeric_limits<int64_t>::max(), 3};
-  tensor_conf.type = at::kFloat;
+  return [](const std::function<void()>& invocation) {
+    EXPECT_THROW(invocation(), Exception);
+  };
+}
+
+const auto compute_max_message_bytes_callable =
+    [](int64_t batch_size, const starpu_server::ModelConfig& model,
+       const std::vector<starpu_server::TensorConfig>&,
+       const std::vector<starpu_server::TensorConfig>&) {
+      std::vector<starpu_server::ModelConfig> models{model};
+      starpu_server::compute_max_message_bytes(batch_size, models);
+    };
+
+const auto compute_model_message_bytes_callable =
+    [](int64_t batch_size, const starpu_server::ModelConfig&,
+       const std::vector<starpu_server::TensorConfig>& inputs,
+       const std::vector<starpu_server::TensorConfig>& outputs) {
+      starpu_server::compute_model_message_bytes(batch_size, inputs, outputs);
+    };
+
+class RuntimeConfigErrorTest
+    : public ::testing::TestWithParam<RuntimeErrorCase> {};
+
+TEST_P(RuntimeConfigErrorTest, ThrowsExpectedException)
+{
+  const auto& param = GetParam();
+
+  int64_t batch_size = 1;
   starpu_server::ModelConfig model;
-  model.inputs = {tensor_conf};
-  EXPECT_THROW(
-      starpu_server::compute_max_message_bytes(1, {model}),
-      starpu_server::MessageSizeOverflowException);
+  std::vector<starpu_server::TensorConfig> inputs;
+  std::vector<starpu_server::TensorConfig> outputs;
+
+  param.prepare_configs(model, inputs, outputs, batch_size);
+
+  const auto invocation = [&]() {
+    param.callable(batch_size, model, inputs, outputs);
+  };
+
+  param.expect_exception(invocation);
 }
 
-TEST(RuntimeConfig, ComputeMaxMessageBytesThrowsOnPerSampleBytesOverflow)
-{
-  starpu_server::TensorConfig tensor_a;
-  tensor_a.dims = {
-      static_cast<int64_t>(std::numeric_limits<size_t>::max() / 4)};
-  tensor_a.type = at::kFloat;
+}  // namespace
 
-  starpu_server::TensorConfig tensor_b;
-  tensor_b.dims = {2};
-  tensor_b.type = at::kFloat;
-  starpu_server::ModelConfig model;
-  model.inputs = {tensor_a};
-  model.outputs = {tensor_b};
-  EXPECT_THROW(
-      starpu_server::compute_max_message_bytes(1, {model}),
-      starpu_server::MessageSizeOverflowException);
-}
+INSTANTIATE_TEST_SUITE_P(
+    RuntimeConfigExceptions, RuntimeConfigErrorTest,
+    ::testing::Values(
+        RuntimeErrorCase{
+            [](starpu_server::ModelConfig& model,
+               std::vector<starpu_server::TensorConfig>&,
+               std::vector<starpu_server::TensorConfig>&, int64_t&) {
+              starpu_server::TensorConfig tensor_conf;
+              tensor_conf.dims = {std::numeric_limits<int64_t>::max(), 3};
+              tensor_conf.type = at::kFloat;
+              model.inputs = {tensor_conf};
+            },
+            make_expect_throw<starpu_server::MessageSizeOverflowException>(),
+            compute_max_message_bytes_callable},
+        RuntimeErrorCase{
+            [](starpu_server::ModelConfig& model,
+               std::vector<starpu_server::TensorConfig>&,
+               std::vector<starpu_server::TensorConfig>&, int64_t&) {
+              starpu_server::TensorConfig tensor_a;
+              tensor_a.dims = {
+                  static_cast<int64_t>(std::numeric_limits<size_t>::max() / 4)};
+              tensor_a.type = at::kFloat;
 
-TEST(RuntimeConfig, ComputeModelMessageBytesThrowsOnBatchSizeOverflow)
-{
-  starpu_server::TensorConfig tensor;
-  tensor.dims = {static_cast<int64_t>(std::numeric_limits<size_t>::max() / 4)};
-  tensor.type = at::kFloat;
+              starpu_server::TensorConfig tensor_b;
+              tensor_b.dims = {2};
+              tensor_b.type = at::kFloat;
 
-  EXPECT_THROW(
-      starpu_server::compute_model_message_bytes(2, {tensor}, {}),
-      starpu_server::MessageSizeOverflowException);
-}
+              model.inputs = {tensor_a};
+              model.outputs = {tensor_b};
+            },
+            make_expect_throw<starpu_server::MessageSizeOverflowException>(),
+            compute_max_message_bytes_callable},
+        RuntimeErrorCase{
+            [](starpu_server::ModelConfig&,
+               std::vector<starpu_server::TensorConfig>& inputs,
+               std::vector<starpu_server::TensorConfig>&, int64_t& batch_size) {
+              batch_size = 2;
 
-TEST(RuntimeConfig, ComputeMaxMessageBytesThrowsOnNegativeInputDimension)
-{
-  starpu_server::TensorConfig tensor_conf;
-  tensor_conf.dims = {-1, 2};
-  tensor_conf.type = at::kFloat;
-  starpu_server::ModelConfig model;
-  model.inputs = {tensor_conf};
-  EXPECT_THROW(
-      starpu_server::compute_max_message_bytes(1, {model}),
-      starpu_server::InvalidDimensionException);
-}
+              starpu_server::TensorConfig tensor;
+              tensor.dims = {
+                  static_cast<int64_t>(std::numeric_limits<size_t>::max() / 4)};
+              tensor.type = at::kFloat;
 
-TEST(RuntimeConfig, ComputeMaxMessageBytesThrowsOnNegativeOutputDimension)
-{
-  starpu_server::TensorConfig tensor_conf;
-  tensor_conf.dims = {2, -1};
-  tensor_conf.type = at::kFloat;
-  starpu_server::ModelConfig model;
-  model.outputs = {tensor_conf};
-  EXPECT_THROW(
-      starpu_server::compute_max_message_bytes(1, {model}),
-      starpu_server::InvalidDimensionException);
-}
+              inputs = {tensor};
+            },
+            make_expect_throw<starpu_server::MessageSizeOverflowException>(),
+            compute_model_message_bytes_callable},
+        RuntimeErrorCase{
+            [](starpu_server::ModelConfig& model,
+               std::vector<starpu_server::TensorConfig>&,
+               std::vector<starpu_server::TensorConfig>&, int64_t&) {
+              starpu_server::TensorConfig tensor_conf;
+              tensor_conf.dims = {-1, 2};
+              tensor_conf.type = at::kFloat;
+              model.inputs = {tensor_conf};
+            },
+            make_expect_throw<starpu_server::InvalidDimensionException>(),
+            compute_max_message_bytes_callable},
+        RuntimeErrorCase{
+            [](starpu_server::ModelConfig& model,
+               std::vector<starpu_server::TensorConfig>&,
+               std::vector<starpu_server::TensorConfig>&, int64_t&) {
+              starpu_server::TensorConfig tensor_conf;
+              tensor_conf.dims = {2, -1};
+              tensor_conf.type = at::kFloat;
+              model.outputs = {tensor_conf};
+            },
+            make_expect_throw<starpu_server::InvalidDimensionException>(),
+            compute_max_message_bytes_callable},
+        RuntimeErrorCase{
+            [](starpu_server::ModelConfig& model,
+               std::vector<starpu_server::TensorConfig>&,
+               std::vector<starpu_server::TensorConfig>&, int64_t& batch_size) {
+              batch_size = -1;
 
-TEST(RuntimeConfig, ComputeMaxMessageBytesThrowsOnNegativeBatchSize)
-{
-  starpu_server::TensorConfig tensor_conf2;
-  tensor_conf2.dims = {1};
-  tensor_conf2.type = at::kFloat;
-  starpu_server::ModelConfig model;
-  model.inputs = {tensor_conf2};
-  EXPECT_THROW(
-      starpu_server::compute_max_message_bytes(-1, {model}),
-      starpu_server::InvalidDimensionException);
-}
-
-TEST(RuntimeConfig, ComputeMaxMessageBytesThrowsOnUnsupportedType)
-{
-  starpu_server::TensorConfig tensor_conf3;
-  tensor_conf3.dims = {1};
-  tensor_conf3.type = at::kComplexFloat;
-  starpu_server::ModelConfig model;
-  model.inputs = {tensor_conf3};
-  EXPECT_THROW(
-      starpu_server::compute_max_message_bytes(1, {model}),
-      starpu_server::UnsupportedDtypeException);
-}
+              starpu_server::TensorConfig tensor_conf;
+              tensor_conf.dims = {1};
+              tensor_conf.type = at::kFloat;
+              model.inputs = {tensor_conf};
+            },
+            make_expect_throw<starpu_server::InvalidDimensionException>(),
+            compute_max_message_bytes_callable},
+        RuntimeErrorCase{
+            [](starpu_server::ModelConfig& model,
+               std::vector<starpu_server::TensorConfig>&,
+               std::vector<starpu_server::TensorConfig>&, int64_t&) {
+              starpu_server::TensorConfig tensor_conf;
+              tensor_conf.dims = {1};
+              tensor_conf.type = at::kComplexFloat;
+              model.inputs = {tensor_conf};
+            },
+            make_expect_throw<starpu_server::UnsupportedDtypeException>(),
+            compute_max_message_bytes_callable}));
