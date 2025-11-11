409e9d400f19d6635478c126e3ee50723cc003a9
daxmawal <jeanfrancoismanutea@gmail.com>
2025-11-11 23:21:23 +0100

    Add tests for latency statistics reporting in InferenceSession

--- tests/unit/core/unit_inference_session.cpp
+++ tests/unit/core/unit_inference_session.cpp
@@ -1,12 +1,15 @@
 #include <gtest/gtest.h>
 
 #include <stdexcept>
+#include <string>
 
 #define private public
 #include "core/inference_session.hpp"
 #undef private
 
+#include "core/inference_runner.hpp"
 #include "core/starpu_setup.hpp"
+#include "test_helpers.hpp"
 #include "utils/runtime_config.hpp"
 
 namespace starpu_server {
@@ -18,6 +21,46 @@ TEST(InferenceSession, LaunchThreadsThrowsWhenWorkerMissing)
   InferenceSession session(opts, starpu);
 
   EXPECT_THROW(session.launch_threads(), std::logic_error);
+}
+
+TEST(InferenceSession, ReportLatencyStatsLogsWhenVerbosityAllows)
+{
+  RuntimeConfig opts;
+  opts.models.resize(1);
+  opts.models[0].inputs = {{"input0", {1}, at::kFloat}};
+  opts.models[0].outputs = {{"output0", {1}, at::kFloat}};
+  opts.verbosity = VerbosityLevel::Stats;
+
+  StarPUSetup starpu(opts);
+  InferenceSession session(opts, starpu);
+
+  InferenceResult result{};
+  result.latency_ms = 1.0;
+  session.results_.push_back(result);
+
+  CaptureStream capture{std::cout};
+  session.report_latency_stats();
+  EXPECT_NE(capture.str().find("Latency stats"), std::string::npos);
+}
+
+TEST(InferenceSession, ReportLatencyStatsSilentWhenVerbosityTooLow)
+{
+  RuntimeConfig opts;
+  opts.models.resize(1);
+  opts.models[0].inputs = {{"input0", {1}, at::kFloat}};
+  opts.models[0].outputs = {{"output0", {1}, at::kFloat}};
+  opts.verbosity = VerbosityLevel::Info;
+
+  StarPUSetup starpu(opts);
+  InferenceSession session(opts, starpu);
+
+  InferenceResult result{};
+  result.latency_ms = 2.0;
+  session.results_.push_back(result);
+
+  CaptureStream capture{std::cout};
+  session.report_latency_stats();
+  EXPECT_EQ(capture.str(), "");
 }
 
 }  // namespace starpu_server
