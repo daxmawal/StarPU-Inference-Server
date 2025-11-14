df2c81b94577964eea8d34a2a1bcc9ea946536c0
daxmawal <jeanfrancoismanutea@gmail.com>
2025-11-14 11:19:04 +0100

    Refactor unit tests: remove obsolete edgecase tests and add new tests for inference task limits

--- tests/unit/core/robustness/edgecases_inference_task.cpp
+++ tests/unit/core/robustness/edgecases_inference_task.cpp
@@ -9,6 +9,18 @@
 #include "core/inference_task.hpp"
 #include "utils/exceptions.hpp"
 
+TEST(InferenceTask_Robustesse, SubmitNullJobThrows)
+{
+  auto job = std::shared_ptr<starpu_server::InferenceJob>(nullptr);
+  torch::jit::script::Module model_cpu;
+  std::vector<torch::jit::script::Module> models_gpu;
+  starpu_server::RuntimeConfig opts;
+
+  starpu_server::InferenceTask task(
+      nullptr, job, &model_cpu, &models_gpu, &opts);
+  EXPECT_THROW(task.submit(), starpu_server::InvalidInferenceJobException);
+}
+
 class InferenceTaskLimitsTest_Robustesse : public ::testing::Test {
  protected:
   void SetUp() override
